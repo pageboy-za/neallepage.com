d439cfa13c1645ac1db9c12ac83eb6dd823aabbf
Nealle Page <nealle.page@gmail.com>
2020-12-28 20:12:15 +0000

    purge enabled

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
   presets: [],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
