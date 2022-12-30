53cc91f2446b6557e9d780dfd9f1dde97867ae81
Nealle Page <nealle.page@gmail.com>
2022-12-30 00:03:56 +0000

    adding vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+  ( 
+    <> 
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+   )
 }
 
 export default MyApp
