e17a601c47d55d77683a02a47c22619692bc6a8c
Nealle Page <nealle.page@gmail.com>
2023-04-07 23:52:05 +0100

    adding umami analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import '../styles/index.css';
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <script async src="https://analytics.umami.is/script.js" data-website-id="76113375-d302-4728-a21e-da3a62ebbe0a"></script>
       <Component {...pageProps} />
       <Analytics />
     </>
