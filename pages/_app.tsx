a42e2dd778e0de8700bc0ae3e732d2765d3900dd
Nealle Page <nealle.page@gmail.com>
2023-04-16 16:33:06 +0100

    fixing the header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
+
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <script async src="https://analytics.umami.is/script.js" data-website-id="76113375-d302-4728-a21e-da3a62ebbe0a"></script>
+    <Script async src="https://analytics.umami.is/script.js" data-website-id="76113375-d302-4728-a21e-da3a62ebbe0a" />
       <Component {...pageProps} />
       <Analytics />
     </>
